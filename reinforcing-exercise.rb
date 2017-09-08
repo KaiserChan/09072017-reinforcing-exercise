bf40535277320b5d0b2ac693ec6324ae9cb28048
KaiserChan <chankaiserkk@gmail.com>
2017-09-08 16:17:51 -0400

    Updated reinforcing exercise ... fixed with method and interpolation instead of using activesupport and inflector

--- reinforcing-exercise.rb
+++ reinforcing-exercise.rb
@@ -1,19 +1,22 @@
-# def ordinal_indicator(num)
-#   num.each do |num|
-#     if num == 1
-#       puts "#{num}" + "st"
-#     elsif num == 2
-#       puts "#{num}" + "nd"
-#     elsif num == 3
-#       puts "#{num}" + "rd"
-#     else
-#       puts "#{num}" + "th"
-#     end
-#   end
-# end
-#
-# array = (1..20).to_a
-# ordinal_indicator(array)
+def ordinal_indicator(num)
+  num.each do |num|
+    if num == 11 || num == 12 || num == 13
+      puts "#{num}" + "th"
+    elsif num % 10 == 1
+      puts "#{num}" + "st"
+    elsif num % 10 == 2
+      puts "#{num}" + "nd"
+    elsif num % 10 == 3
+      puts "#{num}" + "rd"
+    elsif
+      puts "#{num}" + "th"
+    else
+    end
+  end
+end
+
+array = (1..30).to_a
+ordinal_indicator(array)
 
 
 # There is a .ordinalize method in rails (i.e. 5.ordinalize => 5th)...
@@ -21,39 +24,39 @@
 # The following is per in struction at http://api.rubyonrails.org/classes/ActiveSupport/Inflector.html#method-i-ordinalize
 
 
-require "active_support/inflections"
-require "active_support/core_ext/regexp"
-
-module ActiveSupport
-  module Inflector
-    extend self
-
-    def ordinal(num)
-
-      abs_number = num.to_i.abs
-
-      if (11..13).include?(abs_number % 100)
-          "th"
-      else
-        case abs_number % 10
-        when 1; "st"
-        when 2; "nd"
-        when 3; "rd"
-          else    "th"
-        end
-      end
-    end
-
-
-    def ordinalize(number)
-      number.each do |num|
-        puts "#{num}#{ordinal(num)}"
-      end
-    end
-
-    # ordinalize(1)
-    array = (1..30).to_a
-    ordinalize(array)
-
-  end
-end
+# require "active_support/inflections"
+# require "active_support/core_ext/regexp"
+#
+# module ActiveSupport
+#   module Inflector
+#     extend self
+#
+#     def ordinal(num)
+#
+#       abs_number = num.to_i.abs
+#
+#       if (11..13).include?(abs_number % 100)
+#           "th"
+#       else
+#         case abs_number % 10
+#         when 1; "st"
+#         when 2; "nd"
+#         when 3; "rd"
+#           else    "th"
+#         end
+#       end
+#     end
+#
+#
+#     def ordinalize(number)
+#       number.each do |num|
+#         puts "#{num}#{ordinal(num)}"
+#       end
+#     end
+#
+#     # ordinalize(1)
+#     array = (1..30).to_a
+#     ordinalize(array)
+#
+#   end
+# end
